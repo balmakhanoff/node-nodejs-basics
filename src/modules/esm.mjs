177fb91c64ef073cc4b39b9a0db0e66a70876a58
meir <sinogu@yandex.ru>
2025-04-27 01:45:03 +0500

    feat: implement modules

---
+++ src/modules/esm.mjs
@@ -0,0 +1,42 @@
+import {readFile} from 'fs/promises';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import {release, version} from 'os';
+import {createServer as createServerHttp} from 'http';
+import './files/c.cjs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = 3000;
+
+const loadJson = async () => {
+    if (Math.random() > 0.5) {
+        return JSON.parse(await readFile('./files/a.json', 'utf-8'));
+    } else {
+        return JSON.parse(await readFile('./files/b.json', 'utf-8'));
+    }
+}
+
+const getSystemInfo = async () => {
+    console.log(`Release ${release()}`);
+    console.log(`Version ${version()}`);
+    console.log(`Path segment separator is "${path.sep}"`);
+    console.log(`Path to current file is ${__filename}`);
+    console.log(`Path to current directory is ${__dirname}`);
+    console.log(unknownObject);
+}
+
+const unknownObject = await loadJson();
+await getSystemInfo();
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {unknownObject, myServer};
