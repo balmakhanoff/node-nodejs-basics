1a50221f4efa6794229679438621e88abaadaf48
meir <sinogu@yandex.ru>
2025-04-27 23:23:55 +0500

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import {createReadStream, createWriteStream} from "fs";
+import {createGzip} from "zlib";
+import {pipeline} from "stream/promises";
+
 const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const inputFilePath = 'files/fileToCompress.txt';
+    const outputFilePath = 'files/archive.gz';
+
+    await pipeline(
+        createReadStream(inputFilePath),
+        createGzip({level: 9}),
+        createWriteStream(outputFilePath)
+    );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {createGunzip} from 'zlib';
+import {pipeline} from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    // Write your code here
+    const inputFilePath = 'files/archive.gz';
+    const outputFilePath = 'files/fileToCompress.txt';
+
+    await pipeline(
+        createReadStream(inputFilePath),
+        createGunzip(),
+        createWriteStream(outputFilePath)
+    )
 };
 
 await decompress();
