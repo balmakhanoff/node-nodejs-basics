b55b4441ab0640be3ae3a0d4fe8d33154c3f6753
meir <sinogu@yandex.ru>
2025-04-27 16:38:24 +0500

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,27 @@
+import {createHash} from 'crypto';
+import {createReadStream} from 'fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const filePath = 'files/fileToCalculateHashFor.txt';
+        const hash = createHash('sha256');
+        const stream = createReadStream(filePath);
+
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        })
+
+        stream.on('end', () => {
+            const finashHash = hash.digest('hex');
+            console.log(`SHA256 hash: ${finashHash}`);
+        })
+    } catch (err) {
+        stream.on('error', (err) => {
+            console.error('Error reading file:', err);
+            throw err;
+        })
+    }
 };
 
 await calculateHash();
